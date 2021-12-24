34b41b68230dbe457fed3195a5a3e2d27176222a
funtasia233 <yan_qh1964@live.com>
2021-12-24 18:02:56 +0800

    project inited

---
+++ src/defines.rs
@@ -0,0 +1,13 @@
+mod Definitions {
+    // 操作行为定义
+    enum Operation {
+        Buy,
+        Sell,
+    }
+}
+
+mod Definitions {
+    // 市场
+    // 订单数据结构
+    struct OrderItem {}
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn read_csv_file(s: &str) {
+    println!("{}", s);
+}
+
+fn main() {
+    println!("Hello, world!");
+    read_csv_file("test");
+}
+
+// 实现csv文件的读取 并生成对应的struct
